ea69f11a3930bd9d95fe3855b6082840f6dccb95
almogalbalach <almogalbalach@gmail.com>
2014-11-27 23:44:17 +0200

    First commit

---
+++ TaskManager/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace TaskManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonEndTask = new System.Windows.Forms.Button();
+            this.listboxTasks = new System.Windows.Forms.ListBox();
+            this.labelTopTask = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelTaskPeak = new System.Windows.Forms.Label();
+            this.labelTaskPeakNumber = new System.Windows.Forms.Label();
+            this.labelAllocatedMemoryNumber = new System.Windows.Forms.Label();
+            this.labelAllocatedMemory = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::TaskManager.Properties.Resources.brightstork_logo;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(350, 115);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // buttonEndTask
+            // 
+            this.buttonEndTask.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.buttonEndTask.Location = new System.Drawing.Point(279, 274);
+            this.buttonEndTask.Name = "buttonEndTask";
+            this.buttonEndTask.Size = new System.Drawing.Size(76, 28);
+            this.buttonEndTask.TabIndex = 2;
+            this.buttonEndTask.Text = "End Task";
+            this.buttonEndTask.UseVisualStyleBackColor = true;
+            this.buttonEndTask.Click += new System.EventHandler(this.buttonEndTask_Click);
+            // 
+            // listboxTasks
+            // 
+            this.listboxTasks.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.listboxTasks.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listboxTasks.FormattingEnabled = true;
+            this.listboxTasks.Location = new System.Drawing.Point(12, 131);
+            this.listboxTasks.Name = "listboxTasks";
+            this.listboxTasks.Size = new System.Drawing.Size(261, 171);
+            this.listboxTasks.TabIndex = 3;
+            // 
+            // labelTopTask
+            // 
+            this.labelTopTask.AutoSize = true;
+            this.labelTopTask.Location = new System.Drawing.Point(279, 145);
+            this.labelTopTask.Name = "labelTopTask";
+            this.labelTopTask.Size = new System.Drawing.Size(35, 13);
+            this.labelTopTask.TabIndex = 4;
+            this.labelTopTask.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(279, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Top Memory Task:";
+            // 
+            // labelTaskPeak
+            // 
+            this.labelTaskPeak.AutoSize = true;
+            this.labelTaskPeak.Location = new System.Drawing.Point(279, 183);
+            this.labelTaskPeak.Name = "labelTaskPeak";
+            this.labelTaskPeak.Size = new System.Drawing.Size(69, 13);
+            this.labelTaskPeak.TabIndex = 6;
+            this.labelTaskPeak.Text = "Task\'s Peak:";
+            // 
+            // labelTaskPeakNumber
+            // 
+            this.labelTaskPeakNumber.AutoSize = true;
+            this.labelTaskPeakNumber.Location = new System.Drawing.Point(279, 196);
+            this.labelTaskPeakNumber.Name = "labelTaskPeakNumber";
+            this.labelTaskPeakNumber.Size = new System.Drawing.Size(87, 13);
+            this.labelTaskPeakNumber.TabIndex = 7;
+            this.labelTaskPeakNumber.Text = "Allowed Memory:";
+            // 
+            // labelAllocatedMemoryNumber
+            // 
+            this.labelAllocatedMemoryNumber.AutoSize = true;
+            this.labelAllocatedMemoryNumber.Location = new System.Drawing.Point(279, 236);
+            this.labelAllocatedMemoryNumber.Name = "labelAllocatedMemoryNumber";
+            this.labelAllocatedMemoryNumber.Size = new System.Drawing.Size(69, 13);
+            this.labelAllocatedMemoryNumber.TabIndex = 8;
+            this.labelAllocatedMemoryNumber.Text = "Task\'s Peak:";
+            // 
+            // labelAllocatedMemory
+            // 
+            this.labelAllocatedMemory.AutoSize = true;
+            this.labelAllocatedMemory.Location = new System.Drawing.Point(279, 223);
+            this.labelAllocatedMemory.Name = "labelAllocatedMemory";
+            this.labelAllocatedMemory.Size = new System.Drawing.Size(94, 13);
+            this.labelAllocatedMemory.TabIndex = 9;
+            this.labelAllocatedMemory.Text = "Allocated Memory:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 314);
+            this.Controls.Add(this.labelAllocatedMemory);
+            this.Controls.Add(this.labelAllocatedMemoryNumber);
+            this.Controls.Add(this.labelTaskPeakNumber);
+            this.Controls.Add(this.labelTaskPeak);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelTopTask);
+            this.Controls.Add(this.listboxTasks);
+            this.Controls.Add(this.buttonEndTask);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "brightstork v1.0";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button buttonEndTask;
+        private System.Windows.Forms.ListBox listboxTasks;
+        private System.Windows.Forms.Label labelTopTask;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelTaskPeak;
+        private System.Windows.Forms.Label labelTaskPeakNumber;
+        private System.Windows.Forms.Label labelAllocatedMemoryNumber;
+        private System.Windows.Forms.Label labelAllocatedMemory;
+    }
+}
+
---
+++ TaskManager/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+
+namespace TaskManager
+{
+    public partial class Form1 : Form
+    {
+       public Process m_Process = null;
+       private WindowsFormsSynchronizationContext m_GuiThread = null; // this is in order to switch beetwen threads
+       private Thread m_RefreshThread = null; /// in order to close the thread when the application exit's
+       private bool _StopRefreshThread = false; // so refreshh until application exits could run 
+       private Process[] m_process = Process.GetProcesses();
+        public Form1()
+        {
+            InitializeComponent();
+            this.DoubleBuffered = true;
+            refreshTaskList();
+            if (m_GuiThread == null)
+            {
+                m_GuiThread = new WindowsFormsSynchronizationContext(); //rembers that the gui thread is this because we init it from the c'tor            
+            }
+            m_RefreshThread = new Thread(RefreshTaskListThread);//so the thread now which function to activeted once he stratredd
+            m_RefreshThread.Start();   
+        }
+
+
+        private void refreshTaskList()
+        {
+            Process maximumProcess = FindMaximumProcessMemorySize();
+            listboxTasks.Items.Clear();
+            foreach (Process process in Process.GetProcesses())
+            {
+               // process.OutputDataReceived += process_OutputDataReceived;
+                listboxTasks.Items.Add(process.ProcessName + " | " + process.Id + " | " + process.PrivateMemorySize64 / 1000 + " kB");
+            }
+            labelTopTask.Text = maximumProcess.ProcessName + "\n" + maximumProcess.PrivateMemorySize64 / 1000 + " kb";
+            labelTaskPeakNumber.Text = (maximumProcess.PeakPagedMemorySize64 / 1000).ToString() + " kb";
+            labelAllocatedMemoryNumber.Text = (maximumProcess.WorkingSet64 / 1000).ToString() + " kb";   
+
+        }
+
+        //void process_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        //{
+        //    //u can try play with delegate  \ threds \ i gave u the tools
+
+        //    if (sender is Process)
+        //    {
+        //        //write your code here
+        //        //let checks if it works
+
+        //    }
+        //}
+        public void RefreshTaskListThread() // fuck lol :)
+        {
+            while(!_StopRefreshThread) // one of many methods to syncronize beetwen threads , reminder thread is "fire and forrget" opretion  async - task!
+            {
+                if (listboxTasks.Items.Count != 0)
+                {
+                    m_GuiThread.Send((object sender1) =>
+                        {
+                            //sec, lets close visual and restrust?
+                            // almog always preform null and 0 checks if when u dont need , prevent bugs תודה
+                            Process maximumProcess = FindMaximumProcessMemorySize();
+                            listboxTasks.Items.Clear();
+                            foreach (Process process in Process.GetProcesses())
+                            {
+                                listboxTasks.Items.Add(process.ProcessName + " | " + process.Id + " | " + process.PrivateMemorySize64 / 1000 + " kB");
+                             //   process.OutputDataReceived!!!//you seee? nice!
+                                    //so this is the processes listhener? yes its called an event e.g when outdatarecived it tells everyone look what you can do for example
+                            }
+                            labelTopTask.Text = maximumProcess.ProcessName + "\n" + maximumProcess.PrivateMemorySize64 / 1000 + " kb";
+                            labelTaskPeakNumber.Text = (maximumProcess.PeakPagedMemorySize64 / 1000).ToString() + " kb";
+                            labelAllocatedMemoryNumber.Text = (maximumProcess.WorkingSet64 / 1000).ToString() + " kb";
+                            
+                        }, null); //this is method to swtich beetwen Current thread  to gui thread //basacily thread not built to switch but thisthe best workaround 
+                    //using lambda expresions
+                    Thread.Sleep(5000);
+                    //bravo! is there a way to stop the flickring?
+                    //at the windows task manager - its refreshing without flickers or list reload.
+                    //asking from ceriosity, i gussing there orking in diffrent , you should worl with better controler than this, sometimes changiing the control is less expensive, i guessing that each proccer have a delegate(listner) to man it's change and each procces uptades by his owen ! lets check!
+                }
+                else
+                {
+                    Thread.Sleep(1000); // sleep here is good desgin because it's PART of the algorthinm
+                }
+            }
+        }
+
+        public void killTask(string[] i_ArrayOfTasks, string i_StringTask, int i_TaskId)
+        {
+            foreach (Process process in Process.GetProcesses())
+            {
+                if (process.ProcessName == i_StringTask && process.Id == i_TaskId)
+                {
+                    try
+                    {
+                        process.Kill();
+                        MessageBox.Show(process.ProcessName + " terminated.\n" + "Memory cleared: " + (process.PrivateMemorySize64 / 1000).ToString() + " kb", "Bright Stork");
+                    }
+                    catch (Exception exception)
+                    {
+                        MessageBox.Show(exception.Message, "Bright Stork");
+                    }
+                }
+            }
+
+            refreshTaskList();
+        }
+
+        private void buttonEndTask_Click(object sender, EventArgs e)
+        {
+           string[] arrayOfTasks = listboxTasks.SelectedItem.ToString().Split('|');
+           string stringTask = arrayOfTasks[0].Trim();
+           int taskId = Convert.ToInt32(arrayOfTasks[1]);
+           MessageBox.Show(stringTask);
+           killTask(arrayOfTasks, stringTask, taskId);
+        }
+    
+    public Process FindMaximumProcessMemorySize()
+    {    
+        long maximumMemory = 0;
+        Process maximumProcess = null;
+
+        foreach(Process process in Process.GetProcesses())
+        { 
+            if (process.PrivateMemorySize64 > maximumMemory)
+            {
+                maximumMemory = process.PrivateMemorySize64;
+                maximumProcess = process;
+            }
+        }
+
+        return maximumProcess;
+    }
+
+        
+
+    private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+    {
+        // if you have thread in you programs YOU MUST AND I REPEAT YOU MUST! TO CLOSE AND STOP ALL running thread!
+        //sometimes . and sometimes it will cause your application to cruch  + it the right wait to program , not closing all thread is bad programing practice!>
+        //and why doing a special function and not inside destructor?
+        // u can not call disrctur and you never know when the called + it not a function its an event it listen  to the application , got it!!
+        // ok code 
+        if (!_StopRefreshThread)
+        {
+            _StopRefreshThread = true;
+            m_RefreshThread.Join(); //join method  let there be  2 thread A and B if B called join  method thread A waits! for thread b to finish and than cont. this is sync opretion!
+        }
+    }
+  }
+}
